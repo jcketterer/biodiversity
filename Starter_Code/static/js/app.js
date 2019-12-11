8a12d1c1e109a792c56e3ef93454c457e31f1258
JC Ketterer <jcketterer18@gmail.com>
2019-12-11 10:39:20 -0700

    final push

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,174 +1,147 @@
 
+//Run all after window loads 
+window.onload = function(e) {
+    //Using d3 to call data from JSON file
+    d3.json("static/js/samples.json").then((sample) => {
 
-//Getting Data
-const data_url = "https://jcketterer.github.io/biodiversity-/Starter_Code/samples.json";
+        //HTML varibales
+        var personDiv = d3.select("#sample-metadata");
+        var dropdown = document.getElementById("selDataset");
 
-var jsondata;
-var subjects;
-var metaData;
-var samples;
-var filter;
+        //Data variables 
+        var peopleIDS = sample.names;
+        var people = sample.metadata;
+        var sampleResults = sample.samples;
 
-//Define from json
-function defineData(jsondata) {
-    subjects = jsondata.names;
-    metaData = jsondata.metaData;
-    samples = jsondata.samples;
-}
+        //Drop down test
+        var defaultOption = document.createElement('option');
+        defaultOption.text = "Select Option";
+        dropdown.add(defaultOption);
 
-//Dropt Down list 
-function makeSelect(subjects) {
-    console.log("Populate "+subjects.length+" subjects");
-    var dropDown = d3.select('#filter-column').append("select")
-        .attr("id", "subject-selection")
-        .style("background-color", "rgb(133,252,138")
-    ;
-    var options = dropDown.selectAll("option")
-        .data(subjects)
-        .enter()
-        .append("option")
-        .attr("value", function(d) {return d;})
-        .text(function(d) {return d;})
-    ;  
-}
+        //Add all IDS to droup down
+        peopleIDS.forEach(function(id) {
+            var option = document.createElement('option');
+            option.text = id;
+            option.value = id;
+            dropdown.add(option,id);
+        });
+    
+        //Adding event listener to drop down
+        dropdown.addEventListener('change', function(event) {
 
-// Get sample by id 
-var sample_index = -1;
-var sample;
-var meta;
-function getSample(sample, sample_id) {
-    console.log("Searching for:" +sample_id);
-    for (i = 0; i < samples.length; i++) {
-        if (samples[i].id == sample_id){
-            sample_index = 1;
-            sample = sample[i];
-            meta = metaData[i];
-            break;
-        }
-    }
-}
+            //Get values from option 
+            var id = event.target.value;
 
-//Calulate gauge statistics
-var gauge_min = 100; 
-var gauge_max = 0;
-var gauge_mean = 0;
+            //Empty div element
+            personDiv.html("");
 
-function getGaugeStats(metaData) {
-    let washing_frequency = 0;
-    let washing_sum = 0; 
-    for( i = 0; i < metaData.length; i++) {
-        let meta = metaData[i].wfreq;
-        if (meta != null) {
-            washing_frequency += 1;
-            washing_sum += meta;
-            if (meta < gauge_min) {
-                gauge_min = meta;
-            }
-            if (meta > gauge_max) {
-                gauge_max = meta;
-            }
-        }
-    }
-    gauge_mean = washing_sum/washing_frequency;
-}
+            //If Statement for metadata selected and insert ID when found
+            people.forEach(function(person) {
 
-//Display the demographics
-function updateDemographics(meta) {
-    console.log(meta);
-    let subjectid = "ID: " +meta.id;
-    let subject_ethnicity = "Ethnicity: " +meta.ethnicity;
-    let subject_gender = "Gender: " +meta.gender;
-    let subject_age = "Age: " +meta.age;
-    let subject_location = "Location: " +meta.location;
-    let subject_type  = "Type: " +meta.type;
-    let subject_washfreq = "Wash Frequency: " +meta.wfreq;
+                if (id == person.id) {
+                    Object.entries(person).forEach(function([key, value]) {
 
-    d3.select(".subjectid").text(subjectid);
-    d3.select(".ethnicity").text(subject_ethnicity);
-    d3.select(".gender").text(subject_gender);
-    d3.select(".age").text(subject_age);
-    d3.select(".location").text(subject_location);
-    d3.select(".type").text(subject_type);
-}
-
-//Plotting
-
-function buildPlot(filter) {
-    getSample(samples,filter);
-    console.log(sample);
-    updateDemographics(meta);
-    var x = sample.otu_ids;
-    var y = sample.sample_values;
-    var text = sample.otu_labels;
-    var bar_trace = {
-        x: x,
-        y: y,
-        type: "bar",
-        text: text
-    };
-    var bar_data = [bar_trace];
-    var bar_layout = {
-        title: "Sample values by OTU Id Bar Chart"
-    };
-    Plotly.newPlot("bar", bar_data, bar_layout);
-    var bubble_trace = {
-        x: x,
-        y: y,
-        text: text,
-        mode: 'markers',
-        marker: {size: y}
-    };
-    var bubble_data = [bubble_trace];
-    var bubble_layout = {
-        title: "Sample values by OTU Id Bubble Chart",
-        showlegend: false
-    };
-    Plotly.newPlot("bubble",bubble_data,bubble_layout);
-    var washing_frequency = meta.wfreq;
-    var gauge_data = [
-        {
-            domain: { x: [0,1], y: [0,1] },
-            value: washing_frequency,
-            title: { text: "Wash Freq" },
-            type: "indicator",
-            mode: "gauge+number",
-            gauge: {
-                axis: { range: [null, gauge_max]},
-                steps: [
-                    { range: [gauge_min, gauge_mean], color: "lightgray"},
-                    { range: [gauge_mean, gauge_max], color: "rgb(133,252,138)"}
-                ],
-                threshold: {
-                    line: { color: "blue", width: 4 },
-                    thickness: 0.75,
-                    value: gauge_mean
+                        //Display demographic 
+                        var row = personDiv.append("p");
+                        row.text(`${key}: ${value}`);
+                    });
                 }
-            }
-        }
-   ];
-   var gauge_layout = {
-       height: 200,
-       margin: { t: 0, b: 0}
-   };
-   Plotly.newPlot('gauge', gauge_data, gauge_layout);   
+            });
+
+
+            //Search for person sample
+            sampleResults.forEach(function(sample) {
+
+                if (id == sample.id) {
+                    Object.entries(sample).forEach(function([key, value]) {
+
+                        //Display bubble chart
+                        var x_axis = sample.otu_ids;
+                        var y_axis = sample.sample_values;
+                        var size = sample.sample_values;
+                        var color = sample.otu_ids;
+                        var texts = sample.otu_labels;
+
+                        var bubble = {
+                            x: x_axis,
+                            y: y_axis,
+                            text: texts,
+                            mode: `markers`,
+                            marker: {
+                                size: size,
+                                color: color
+                            }
+                        };
+
+                        var layout = {
+                            title: "Belly Button Bacteria",
+                            xaxis: { title: "OTU ID" }};
+                        
+                        Plotly.newPlot("bubble", [bubble], layout);
+                    });
+                }
+            });
+            // Scan for sample results for bar chart
+            sampleResults.forEach(function(sample) {
+
+                if (id == sample.id) {
+                    Object.entries(sample).forEach(function([key,value]){
+
+                        //Display bar chart from data chosen
+                        var xaxis = sample.sample_values.slice(0,10);
+                        var yaxis = sample.otu_ids.slice(0,10);
+                        var barHover = sample.otu_labels.slice(0,10);
+
+                        var bar = {
+                            type: 'bar',
+                            x: xaxis,
+                            y: yaxis,
+                            hovertext: barHover,
+                            orientation: 'h'
+                        };
+
+                        var layout = {
+                            title: "Belly Button Bar Chart"
+                        };
+
+                        Plotly.newPlot("bar", [bar], layout);
+                    });
+                }
+            });
+
+            //Scan for sample results for pie chart
+            sampleResults.forEach(function(sample) { 
+                
+                if (id == sample.id) {
+                    Object.entries(sample).forEach(function([key, value]){
+
+                        //Display pie chart
+                        var pieValues = sample.sample_values.slice(0,10);
+                        var pieLabels = sample.otu_ids.slice(0,10);
+                        var pieHover = sample.otu_labels.slice(0,10);
+
+                        var pie = {
+                            type: 'pie',
+                            labels: pieValues,
+                            values: pieLabels,
+                            hovertext: pieHover
+                        };
+
+                        var layout = { title: "Pie Chart" };
+                                                
+                        Plotly.newPlot("pie", [pie], layout)
+                                    
+                    });
+                }
+            });    
+        });
+    });
 }
 
-d3.json(data_url).then(data => {
-    console.log(data);
-    jsondata = data;
-    defineData(jsondata);
-    getGaugeStats(metaData);
-    makeSelect(subjects);
-    var selectSubject = d3.select("#subject-selection")
-    selectSubject.on("change", function() {
-        filter = d3.event.target.value;
-        console.log(filter+" selected");
-        buildPlot(filter);
-    });
-});
+
 
 // function buildMetadata(samples) {
-//     var metaurl = "samples.json";
+//     var metaurl = "static/js/samples.json";
 //     d3.json(metaurl).then(function (sample) {
 //         console.log(sample);
 //         var sample_metadata = d3.select("#sample-metadata");
@@ -179,9 +152,9 @@ d3.json(data_url).then(data => {
 //         });
 //     });
 // };
-
+// buildMetadata();
 // function buildCharts(samples) {
-//     var url = "samples.json"
+//     var url = "static/js/samples.json"
 //     d3.json(url).then(function (d) {
 //         var bx = d.otu_ids;
 //         var by = d.sample_values;
